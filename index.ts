e6c5e60d6b813bac0415140c229e209a75321ea9
mockingjae <macqueen1001@gmail.com>
2024-07-12 04:36:09 +0900

    Feat: BatchJob support

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * as messageRelay from "./MessageRelay/MessageRelayer";
+export * as batchJob from "./MessageRelay/BatchJob";
 export * as p3saga from "./Saga/index";
 export * as uow from "./UnitOfWork/main";
 export * as uowMemory from "./UnitOfWork/inMemory";
