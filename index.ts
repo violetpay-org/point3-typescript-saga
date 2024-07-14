f7e50fb3dac2adbebc55843cc47c68c9af09e193
mockingjae <macqueen1001@gmail.com>
2024-07-14 15:49:15 +0900

    Chore: Made messageRelayChannel available

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * as messageRelay from "./MessageRelay/MessageRelayer";
+export * as messageRelayChannel from "./MessageRelay/Channel";
 export * as batchJob from "./MessageRelay/BatchJob";
 export * as p3saga from "./Saga/index";
 export * as uow from "./UnitOfWork/main";
