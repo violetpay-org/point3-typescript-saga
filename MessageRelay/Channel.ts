21a497ed9a952b9a96e30ac253de7ecc14a58cd1
Jaewook Lee <wookboy00@naver.com>
2024-09-12 11:55:09 +0900

    Fix: BaseRemoteCommandChannel should implement ChannelFromRelayer

--- MessageRelay/Channel.ts
+++ MessageRelay/Channel.ts
@@ -1,7 +1,7 @@
-import { AbstractSagaMessage } from "Saga/Endpoint";
-import { endpoint, api, saga } from "../Saga/index";
-import { TxContext } from "UnitOfWork/main";
-import { Mutex } from "async-mutex";
+import { AbstractSagaMessage } from 'Saga/Endpoint';
+import { endpoint, api, saga } from '../Saga/index';
+import { TxContext } from 'UnitOfWork/main';
+import { Mutex } from 'async-mutex';
 
 export interface ChannelFromMessageRelay<C extends AbstractSagaMessage, Tx extends TxContext>
     extends endpoint.Channel<C> {
@@ -14,13 +14,15 @@ export class ChannelRegistryForMessageRelay<Tx extends TxContext> {
 
     constructor(channelRegistry: api.ChannelRegistry) {
         this._channelRegistry = channelRegistry;
-        this._channelMutex = new Mutex;
+        this._channelMutex = new Mutex();
     }
 
     // This is a type guard
     // This function checks if the channel is a ChannelFromMessageRelay
-    private isChannelFromMessageRelay<M extends endpoint.AbstractSagaMessage>(channel: endpoint.Channel<M>): channel is ChannelFromMessageRelay<M, Tx> {
-        return "getRepository" in channel;
+    private isChannelFromMessageRelay<M extends endpoint.AbstractSagaMessage>(
+        channel: endpoint.Channel<M>,
+    ): channel is ChannelFromMessageRelay<M, Tx> {
+        return 'getRepository' in channel;
     }
 
     public async getChannelByName(channelName: string): Promise<ChannelFromMessageRelay<AbstractSagaMessage, Tx>> {
@@ -40,24 +42,20 @@ export class ChannelRegistryForMessageRelay<Tx extends TxContext> {
 
     public async getChannels(): Promise<ChannelFromMessageRelay<AbstractSagaMessage, Tx>[]> {
         try {
-            return this._channelRegistry.getChannels()
-                .filter(this.isChannelFromMessageRelay);
+            return this._channelRegistry.getChannels().filter(this.isChannelFromMessageRelay);
         } finally {
             this._channelMutex.release();
         }
     }
 }
 
-export abstract class BaseLocalResponseChannel<
-    R extends endpoint.Response,
-    Tx extends TxContext
-> extends api.ChannelToSagaRegistry<R, Tx> implements ChannelFromMessageRelay<R, Tx> {
+export abstract class BaseLocalResponseChannel<R extends endpoint.Response, Tx extends TxContext>
+    extends api.ChannelToSagaRegistry<R, Tx>
+    implements ChannelFromMessageRelay<R, Tx>
+{
     private _repository: endpoint.AbstractMessageRepository<R, Tx>;
 
-    constructor(
-        sagaRegistry: api.SagaRegistry<Tx>,
-        repository: endpoint.AbstractMessageRepository<R, Tx>
-    ) {
+    constructor(sagaRegistry: api.SagaRegistry<Tx>, repository: endpoint.AbstractMessageRepository<R, Tx>) {
         super(sagaRegistry);
         this._repository = repository;
     }
@@ -68,10 +66,13 @@ export abstract class BaseLocalResponseChannel<
 }
 
 export abstract class BaseRemoteCommandChannel<
-    S extends saga.SagaSession,
-    A extends endpoint.CommandArguments,
-    Tx extends TxContext
-> extends endpoint.AbstractChannel<endpoint.Command<S, A>> {
+        S extends saga.SagaSession,
+        A extends endpoint.CommandArguments,
+        Tx extends TxContext,
+    >
+    extends endpoint.AbstractChannel<endpoint.Command<S, A>>
+    implements ChannelFromMessageRelay<endpoint.Command<S, A>, Tx>
+{
     private _repository: endpoint.CommandRepository<endpoint.Command<S, A>, Tx>;
 
     constructor(repository: endpoint.CommandRepository<endpoint.Command<S, A>, Tx>) {
